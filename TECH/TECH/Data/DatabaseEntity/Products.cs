6f1f2bbc16a6077c445f0fc7fca9190cc21401c9
lexuanhai <emcuahai@gmail.com>
2023-02-07 16:30:37 +0700

    test

---
+++ TECH/TECH/Data/DatabaseEntity/Products.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TECH.SharedKernel;
+
+namespace TECH.Data.DatabaseEntity
+{
+    [Table("products")]
+    public class Products : DomainEntity<int>
+    {
+        [Column(TypeName = "nvarchar(250)")]
+        public string? name { get; set; }
+        public int? category_id { get; set; }
+        [ForeignKey("category_id")]
+        public Category? Category { get; set; }
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal? price_sell { get; set; }
+
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal? price_reduced { get; set; }
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal? price_import { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? trademark { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
+        public string? images { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
+        public string? sizes { get; set; }
+
+        public int? trademarkId { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? description { get; set; }        
+        
+        public int? status { get; set; }
+        public int? ishidden { get; set; }
+        
+    }
+}
